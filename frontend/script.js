0d33cc6f1b48af0dc3dedcfe228014b526108ed9
kriszti0230 <kriszti0230@gmail.com>
2022-06-02 09:13:01 +0200

    init

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,14 @@
-console.log(x)
+const googleSearchButton = document.querySelector("#btn-google-search");
+
+googleSearchButton.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    const form = document.querySelector("#form-google-search");
+    const searchTerm = form.querySelector("input[name='q']").value;
+
+    if (searchTerm === "") {
+        alert("Írj már be valamit!!");
+    } else {
+        form.submit();
+    }
+});
